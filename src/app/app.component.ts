81f244f1b3402ed2564bed80b82f0a5bc69b8397
Jose Almoguera <jalmoguera@bravent.net>
2021-02-08 13:17:29 +0100

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,31 +40,87 @@ export class AppComponent {
       precio: 35000,
     },
     {
-      id: '3',
+      id: '4',
       marca: 'Toyota',
       modelo: 'Auris',
       puertas: 5,
       tipoDeCoche: 'familiar',
       potencia: 1400,
       oferta: false,
-      visible: false,
+      visible: true,
       vendido: false,
       fecha: '2021-01-11',
       precio: 15000
     }];
 
     this.coches.push({
-      id: '1',
+      id: '5',
       marca: 'Mercedes',
       modelo: 'Z',
       puertas: 3,
       tipoDeCoche: 'todoterreno',
       potencia: 3000,
       oferta: false,
+      visible: true,
+      vendido: false,
+      fecha: '2021/01/10',
+      precio: 50000,
+
+    });
+    this.coches.push({
+      id: '6',
+      marca: 'BMW',
+      modelo: 'Serie 5',
+      puertas: 3,
+      tipoDeCoche: 'Deportivo',
+      potencia: 3000,
+      oferta: false,
       visible: false,
       vendido: false,
       fecha: '2021/01/10',
       precio: 50000,
+
+    });
+    this.coches.push({
+      id: '9',
+      marca: 'SEAT',
+      modelo: 'Leon',
+      puertas: 3,
+      tipoDeCoche: 'Familiar',
+      potencia: 3000,
+      oferta: false,
+      visible: true,
+      vendido: true,
+      fecha: '2021/01/10',
+      precio: 50000,
+
+    });
+    this.coches.push({
+      id: '8',
+      marca: 'Opel',
+      modelo: 'Corsa',
+      puertas: 5,
+      tipoDeCoche: 'Turismo',
+      potencia: 3000,
+      oferta: false,
+      visible: false,
+      vendido: false,
+      fecha: '2021/01/10',
+      precio: 15000,
+
+    });
+    this.coches.push({
+      id: '10',
+      marca: 'Ford',
+      modelo: 'Mondeo',
+      puertas: 5,
+      tipoDeCoche: 'Turismo',
+      potencia: 3000,
+      oferta: false,
+      visible: false,
+      vendido: false,
+      fecha: '2021/01/10',
+      precio: 22000,
 
     });
   }
